c6f65a85db9d955df981787b5ed325328ac4aafb
GUILHERME TRAVASSOS OLIVEIRA PRADO DE GODOY <guilherme.topgodoy@senacsp.edu.br>
2023-11-10 17:16:26 -0300

    aula 03 mustache e condicional de html

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,35 @@ import {Router, Request, Response} from 'express'
 const router = Router()
 
 //criando a primeira rota 
-router.get("/", (req,res) => {
-    res.send("Olá mundo!")
+router.get("/", (req:Request,res:Response) => {
+
+    let user = {
+        name: 'Guilherme',
+        age: 29,
+        showAge: false
+    }
+
+    if (user.age >= 18) {
+        user.showAge = true
+    } 
+
+    //let user: string = 'Guilherme' 
+    res.render('home', {
+        user,
+        produtos: [
+            {titulo: 'Produto X', preco: 200},
+            {titulo: 'Produto Y', preco: 500},
+            {titulo: 'Produto Z', preco: 450}
+        ]            
+    })
 })
 
 router.get("/contato", (req:Request, res:Response) =>{
-    res.send("Esta é a página de contato")
+    res.render('contato')
+})
+
+router.get("/sobre", (req:Request, res:Response) =>{
+    res.render('sobre')
 })
 //rota estática 
 router.get("/noticia/titulo-da-noticia", (req:Request, res:Response) =>{
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,27 @@
 //importando biblioteca EXPRESS
 import express, {Request, Response} from "express"
-import { Server } from "http"
+import path from "path"
 //importando nosso arquivo index.ts
 import mainRoutes from './routes/index'
+import mustache from 'mustache-express'
 
 //usando EXPRESS
 const server = express()
 
+//configurando o mustache
+server.set('view engine', 'mustache')
+//configurando caminho da pasta view
+server.set('views', path.join(__dirname, 'views'))
+server.engine('mustache', mustache())
+//configurando caminho da pasta public
+server.use(express.static(path.join(__dirname, '../public')))
+
 server.use(mainRoutes)
+
+//criando a página não encontrada
+server.use((req:Request, res:Response) => {
+    res.status(404).send('Página não encontrada')
+})
 
 //gerando o servidor na porta 3000
 server.listen(3000)
